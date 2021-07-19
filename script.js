fa0c9e6b61f0f0daf8317bba3203b875c71eae32
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-07-19 15:50:12 +0100

    completed basic exercise

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 var myArgs = process.argv.slice(2);
 
 const a = parseInt(myArgs[0]);
-const b = parseInt(myArgs[1]);
+const b = parseInt(myArgs[2]);
 
-if (myArgs[2] === "+") {
+if (isInteger(a) === false || isInteger(b) === false) {
+  console.log("Please enter a number");
+} else if (
+  myArgs[1] !== "add" ||
+  myArgs[1] !== "minus" ||
+  myArgs[1] !== "times" ||
+  myArgs[1] !== "divided"
+) {
+  console.log("Please enter a valid operator");
+}
+
+if (myArgs[1] === "add") {
   console.log(a + b);
 }
 
-if (myArgs[2] === "-") {
+if (myArgs[1] === "minus") {
   console.log(a - b);
 }
 
-if (myArgs[2] === "x") {
+if (myArgs[1] === "times") {
   console.log(a * b);
 }
 
-if (myArgs[2] === "/") {
+if (myArgs[1] === "divided") {
   console.log(a / b);
 }
